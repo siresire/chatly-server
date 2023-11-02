a0e4b40393d4409c38a5e695cdaf4321446cb114
Godfrey Bosire <godfreybosire@Godfreys-MacBook-Pro.local>
2023-11-02 20:37:02 +0300

    #

--- README.md
+++
@@ -1,2 +0,0 @@
-# chatly-server
-# chatly-server
--- server.js
+++ server.js
@@ -1,271 +1,98 @@
-const mongoose = require("mongoose");
-const jwt = require("jsonwebtoken");
-const dotenv = require("dotenv");
-dotenv.config({ path: "./config.env" });
+const express = require('express');
+const app = express();
+const userRoutes = require('./routes/userRoutes')
+const User = require('./models/User');
+const Message = require('./models/Message')
+const rooms = ['general', 'tech', 'finance', 'crypto'];
+const cors = require('cors');
 
-process.on("uncaughtException", (err) => {
-  console.log(err);
-  console.log("UNCAUGHT Exception! Shutting down ...");
-  process.exit(1); // Exit Code 1 indicates that a container shut down, either because of an application failure.
-});
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(cors());
 
-const app = require("./app");
+app.use('/users', userRoutes)
+require('./connection')
 
-const http = require("http");
-const server = http.createServer(app);
-
-const { Server } = require("socket.io"); // Add this
-const { promisify } = require("util");
-const User = require("./models/user");
-const FriendRequest = require("./models/friendRequest");
-const OneToOneMessage = require("./models/OneToOneMessage");
-
-
-// Add this
-// Create an io server and allow for CORS from http://localhost:3000 with GET and POST methods
-const io = new Server(server, {
+const server = require('http').createServer(app);
+const PORT = 5001;
+const io = require('socket.io')(server, {
   cors: {
-    origin: "*",
-    methods: ["GET", "POST"],
-  },
-});
-
-const DB = process.env.DATABASE.replace(
-  "<PASSWORD>",
-  process.env.DATABASE_PASSWORD
-);
-
-mongoose
-  .connect(DB, {
-    // useNewUrlParser: true, // The underlying MongoDB driver has deprecated their current connection string parser. Because this is a major change, they added the useNewUrlParser flag to allow users to fall back to the old parser if they find a bug in the new parser.
-    // useCreateIndex: true, // Again previously MongoDB used an ensureIndex function call to ensure that Indexes exist and, if they didn't, to create one. This too was deprecated in favour of createIndex . the useCreateIndex option ensures that you are using the new function calls.
-    // useFindAndModify: false, // findAndModify is deprecated. Use findOneAndUpdate, findOneAndReplace or findOneAndDelete instead.
-    // useUnifiedTopology: true, // Set to true to opt in to using the MongoDB driver's new connection management engine. You should set this option to true , except for the unlikely case that it prevents you from maintaining a stable connection.
-  })
-  .then((con) => {
-    console.log("DB Connection successful");
-  });
-
-const port = process.env.PORT || 8000;
-
-server.listen(port, () => {
-  console.log(`App running on port ${port} ...`);
-});
-
-// Add this
-// Listen for when the client connects via socket.io-client
-io.on("connection", async (socket) => {
-  console.log(socket);
-  console.log(socket.handshake.query);
-  console.log(JSON.stringify(socket.handshake.query));
-  const user_id = socket.handshake.query["user_id"];
-
-  console.log(`User connected ${socket.id}`);
-  const socket_id = socket.id;
-
-  if (Boolean(user_id)) {
-    User.findByIdAndUpdate(user_id, { socket_id, status: "Online"})
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST']
   }
-  // wrting my own socketes even listner 
+})
 
 
-  socket.on("friend_request", async (data) => {
+async function getLastMessagesFromRoom(room){
+  let roomMessages = await Message.aggregate([
+    {$match: {to: room}},
+    {$group: {_id: '$date', messagesByDate: {$push: '$$ROOT'}}}
+  ])
+  return roomMessages;
+}
 
-    console.log(data.to);
+function sortRoomMessagesByDate(messages){
+  return messages.sort(function(a, b){
+    let date1 = a._id.split('/');
+    let date2 = b._id.split('/');
 
-    
-    const to = await User.findById(data.to).select("socket_id");
-    const from = await User.findById(data.from).select("socket_id");
+    date1 = date1[2] + date1[0] + date1[1]
+    date2 =  date2[2] + date2[0] + date2[1];
 
-    // create a friend request
-    await FriendRequest.create({
-      sender: data.from,
-      recipient: data.to,
-    });
-    // emit event request received to recipient
-    io.to(to?.socket_id).emit("new_friend_request", {
-      message: "New friend request received",
-    });
-    io.to(from?.socket_id).emit("request_sent", {
-      message: "Request Sent successfully!",
-    });
-  });
+    return date1 < date2 ? -1 : 1
+  })
+}
 
-  socket.on("accept_request", async (data) => {
-    // accept friend request => add ref of each other in friends array
-    console.log(data);
-    const request_doc = await FriendRequest.findById(data.request_id);
+// socket connection
 
-    console.log(request_doc);
+io.on('connection', (socket)=> {
 
-    const sender = await User.findById(request_doc.sender);
-    const receiver = await User.findById(request_doc.recipient);
+  socket.on('new-user', async ()=> {
+    const members = await User.find();
+    io.emit('new-user', members)
+  })
 
-    sender.friends.push(request_doc.recipient);
-    receiver.friends.push(request_doc.sender);
+  socket.on('join-room', async(newRoom, previousRoom)=> {
+    socket.join(newRoom);
+    socket.leave(previousRoom);
+    let roomMessages = await getLastMessagesFromRoom(newRoom);
+    roomMessages = sortRoomMessagesByDate(roomMessages);
+    socket.emit('room-messages', roomMessages)
+  })
 
-    await receiver.save({ new: true, validateModifiedOnly: true });
-    await sender.save({ new: true, validateModifiedOnly: true });
+  socket.on('message-room', async(room, content, sender, time, date) => {
+    const newMessage = await Message.create({content, from: sender, time, date, to: room});
+    let roomMessages = await getLastMessagesFromRoom(room);
+    roomMessages = sortRoomMessagesByDate(roomMessages);
+    // sending message to room
+    io.to(room).emit('room-messages', roomMessages);
+    socket.broadcast.emit('notifications', room)
+  })
 
-    await FriendRequest.findByIdAndDelete(data.request_id);
-
-    // delete this request doc
-    // emit event to both of them
-
-    // emit event request accepted to both
-    io.to(sender?.socket_id).emit("request_accepted", {
-      message: "Friend Request Accepted",
-    });
-    io.to(receiver?.socket_id).emit("request_accepted", {
-      message: "Friend Request Accepted",
-    });
-  });
- 
-
-  socket.on("get_direct_conversations", async ({ user_id }, callback) => {
-    const existing_conversations = await OneToOneMessage.find({
-      participants: { $all: [user_id] },
-    }).populate("participants", "firstName lastName avatar _id email status");
-
-    // db.books.find({ authors: { $elemMatch: { name: "John Smith" } } })
-
-    console.log(existing_conversations);
-
-    callback(existing_conversations);
-  });
-
-  socket.on("start_conversation", async (data) => {
-    // data: {to: from:}
-
-    const { to, from } = data;
-
-    // check if there is any existing conversation
-
-    const existing_conversations = await OneToOneMessage.find({
-      participants: { $size: 2, $all: [to, from] },
-    }).populate("participants", "firstName lastName _id email status");
-
-    console.log(existing_conversations[0], "Existing Conversation");
-
-    // if no => create a new OneToOneMessage doc & emit event "start_chat" & send conversation details as payload
-    if (existing_conversations.length === 0) {
-      let new_chat = await OneToOneMessage.create({
-        participants: [to, from],
-      });
-
-      new_chat = await OneToOneMessage.findById(new_chat).populate(
-        "participants",
-        "firstName lastName _id email status"
-      );
-
-      console.log(new_chat);
-
-      socket.emit("start_chat", new_chat);
-    }
-    // if yes => just emit event "start_chat" & send conversation details as payload
-    else {
-      socket.emit("start_chat", existing_conversations[0]);
-    }
-  });
-
-  socket.on("get_messages", async (data, callback) => {
+  app.delete('/logout', async(req, res)=> {
     try {
-      const { messages } = await OneToOneMessage.findById(
-        data.conversation_id
-      ).select("messages");
-      callback(messages);
-    } catch (error) {
-      console.log(error);
+      const {_id, newMessages} = req.body;
+      const user = await User.findById(_id);
+      user.status = "offline";
+      user.newMessages = newMessages;
+      await user.save();
+      const members = await User.find();
+      socket.broadcast.emit('new-user', members);
+      res.status(200).send();
+    } catch (e) {
+      console.log(e);
+      res.status(400).send()
     }
-  });
+  })
 
-  // Handle incoming text/link messages
-  socket.on("text_message", async (data) => {
-    console.log("Received message:", data);
+})
 
-    // data: {to, from, text}
 
-    const { message, conversation_id, from, to, type } = data;
+app.get('/rooms', (req, res)=> {
+  res.json(rooms)
+})
 
-    const to_user = await User.findById(to);
-    const from_user = await User.findById(from);
 
-    // message => {to, from, type, created_at, text, file}
-
-    const new_message = {
-      to: to,
-      from: from,
-      type: type,
-      created_at: Date.now(),
-      text: message,
-    };
-
-    // fetch OneToOneMessage Doc & push a new message to existing conversation
-    const chat = await OneToOneMessage.findById(conversation_id);
-    chat.messages.push(new_message);
-    // save to db`
-    await chat.save({ new: true, validateModifiedOnly: true });
-
-    // emit incoming_message -> to user
-
-    io.to(to_user?.socket_id).emit("new_message", {
-      conversation_id,
-      message: new_message,
-    });
-
-    // emit outgoing_message -> from user
-    io.to(from_user?.socket_id).emit("new_message", {
-      conversation_id,
-      message: new_message,
-    });
-  });
-
-  // handle Media/Document Message
-  socket.on("file_message", (data) => {
-    console.log("Received message:", data);
-
-    // data: {to, from, text, file}
-
-    // Get the file extension
-    const fileExtension = path.extname(data.file.name);
-
-    // Generate a unique filename
-    const filename = `${Date.now()}_${Math.floor(
-      Math.random() * 10000
-    )}${fileExtension}`;
-
-    // upload file to AWS s3
-
-    // create a new conversation if its dosent exists yet or add a new message to existing conversation
-
-    // save to db
-
-    // emit incoming_message -> to user
-
-    // emit outgoing_message -> from user
-  });
-
- 
-  // -------------- HANDLE SOCKET DISCONNECTION ----------------- //
-
-  socket.on("end", async (data) => {
-    // Find user by ID and set status as offline
-
-    if (data.user_id) {
-      await User.findByIdAndUpdate(data.user_id, { status: "Offline" });
-    }
-
-    // broadcast to all conversation rooms of this user that this user is offline (disconnected)
-
-    console.log("closing connection");
-    socket.disconnect(0);
-  });
-});
-
-process.on("unhandledRejection", (err) => {
-  console.log(err);
-  console.log("UNHANDLED REJECTION! Shutting down ...");
-  server.close(() => {
-    process.exit(1); //  Exit Code 1 indicates that a container shut down, either because of an application failure.
-  });
-});
+server.listen(PORT, ()=> {
+  console.log('listening to port', PORT)
+})
