f471c871caec1af0c84b6cacf1000f6199e06320
Godfrey Bosire <godfreybosire@Godfreys-MacBook-Pro.local>
2023-11-02 21:33:12 +0300

    #

--- connection.js
+++ connection.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PW}@cluster0.s11qz.mongodb.net/chatAppMern?retryWrites=true&w=majority`, ()=> {
-  console.log('connected to mongodb')
+// mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PW}@cluster0.trdujnc.mongodb.net/chatapp?retryWrites=true&w=majority`, ()=> {
+//   console.log('connected to mongodb ...')
+// })
+
+mongoose.connect("mongodb+srv://siresire:siresire@cluster0.xjh8kjw.mongodb.net/?retryWrites=true&w=majority", ()=> {
+  console.log('connected to mongodb ...')
+
 })
